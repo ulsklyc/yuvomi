49f976a2b1b498e5346e687bbce10e4f06d7a79e
Ulas Kalayci <ulas.kalayci@icloud.com>
2026-09-25 13:38:52 +0200

    fix(sw): precache the documents facet and expiry helpers
    Both are imported by pages/documents.js; test:sw-precache flagged them as missing from the precached module graph.

--- public/sw.js
+++ public/sw.js
@@ -127,6 +127,8 @@ const APP_SHELL = [
   '/utils/day-label.js',
   '/utils/currency-codes.js',
   '/utils/calendar-delete.js',
+  '/utils/document-expiry.js',
+  '/utils/document-facets.js',
   '/utils/document-folder-delete.js',
   '/utils/document-preview.js',
   '/utils/event-color.js',
